4dc22bc99272512d3c7deb43ec8d4e3c0455d761
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 15:26:07 +0000

    create shipment queries

---
+++ Shop.Application/Shipments/Queries/GetShipmentByIdHandler.cs
@@ -0,0 +1,23 @@
+namespace Shop.Application.Shipments.Queries;
+
+using Interfaces;
+using Domain.Entities;
+using Domain.Errors;
+
+public class GetShipmentByIdHandler : IRequestHandler<GetShipmentByIdQuery, Result<Shipment>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetShipmentByIdHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<Shipment>> Handle(GetShipmentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var shipment = await _context.Shipments.FirstOrDefaultAsync(s =>
+            s.Id == new ShipmentId(request.Id), cancellationToken);
+
+        return shipment != null ? Result.Ok(shipment) : Result.Fail(ShipmentError.NotFound(request.Id));
+    }
+}
