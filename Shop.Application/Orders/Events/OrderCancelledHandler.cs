8a4df65b1209fdaac407a6b75c9d278ea6aa0783
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 14:56:56 +0000

    create order events

---
+++ Shop.Application/Orders/Events/OrderCancelledHandler.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+
+namespace Shop.Application.Orders.Events;
+
+using Interfaces;
+using Domain.Events;
+
+public class OrderCancelledHandler : INotificationHandler<OrderCancelled>
+{
+    private readonly ILogger<OrderConfirmedHandler> _logger;
+    private readonly IEmailService _emailService;
+
+    public OrderCancelledHandler(ILogger<OrderConfirmedHandler> logger, IEmailService emailService)
+    {
+        _logger = logger;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(OrderCancelled notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("{@DateTime}: Started {@Request}",
+            nameof(OrderCancelledHandler), DateTime.UtcNow);
+        
+        
+        
+        _logger.LogInformation("{@DateTime}: Completed {@Request}",
+            nameof(OrderCancelledHandler), DateTime.UtcNow);
+    }
+}
