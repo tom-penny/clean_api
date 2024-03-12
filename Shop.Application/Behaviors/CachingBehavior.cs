7beeab19e1db691676a602d1ef4906cbf9ea1192
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 18:59:45 +0000

    create caching behavior

---
+++ Shop.Application/Behaviors/CachingBehavior.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Logging;
+
+namespace Shop.Application.Behaviors;
+
+using Interfaces;
+
+// Pipeline interceptor for Mediatr request caching.
+
+public class CachingBehavior<TRequest, TResponse> :
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICachedQuery<TResponse>
+    where TResponse : IResultBase
+{
+    private readonly ICacheService _cacheService;
+
+    public CachingBehavior(ICacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var result = await _cacheService.GetAsync<TResponse>(request.Key);
+
+        if (result.IsSuccess) return result.Value;
+
+        var response = await next();
+
+        await _cacheService.SetAsync(request.Key, response, request.Expiry);
+
+        return response;
+    }
+}
