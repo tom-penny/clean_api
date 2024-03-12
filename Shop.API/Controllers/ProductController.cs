ae6fe9ee0e3abc8b293b84547534e6977eb13595
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 16:56:46 +0000

    create product controller

---
+++ Shop.API/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Shop.API.Controllers;
+
+using Mappings;
+using Extensions;
+using Models.Requests;
+using Application.Products.Commands;
+using Application.Products.Queries;
+
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet("/api/products/{id}")]
+    public async Task<IActionResult> GetProduct([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetProductByIdQuery(id);
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        return result.IsSuccess ? Ok(result.Value.ToResponse()) : result.ToProblem();
+    }
+    
+    [HttpGet("/api/products")]
+    public async Task<IActionResult> GetAllProducts([FromQuery] GetAllProductsRequest request,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetAllProductsQuery
+        {
+            SortBy = request.SortBy,
+            OrderBy = request.OrderBy,
+            Page = request.Page,
+            Size = request.Size
+        };
+        
+        var result = await _mediator.Send(query, cancellationToken);
+        
+        var products = result.Value.Select(o => o.ToResponse()).ToList();
+        
+        return Ok(new { products });
+    }
+
+    [Authorize(Policy = "RequireAdmin")]
+    [HttpPost("/api/products")]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateProductCommand
+        {
+            Name = request.Name,
+            Stock = request.Stock,
+            Price = request.Price,
+            CategoryIds = request.CategoryIds
+        };
+        
+        var result = await _mediator.Send(command, cancellationToken);
+
+        if (result.IsFailed) return result.ToProblem();
+        
+        var product = result.Value.ToResponse();
+
+        return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+    }
+    
+    [Authorize(Policy = "RequireAdmin")]
+    [HttpPut("/api/products/{id}")]
+    public async Task<IActionResult> UpdateProduct([FromRoute] Guid id,
+        [FromBody] UpdateProductRequest request, CancellationToken cancellationToken)
+    {
+        var command = new UpdateProductCommand
+        {
+            Id = id,
+            Name = request.Name,
+            Stock = request.Stock,
+            Price = request.Price,
+            CategoryIds = request.CategoryIds
+        };
+        
+        var result = await _mediator.Send(command, cancellationToken);
+        
+        return result.IsSuccess ? Ok() : result.ToProblem();
+    }
+}
