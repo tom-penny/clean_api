3d89aa056b31370fa496c1cb71d456f32a52f4cb
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 15:54:41 +0000

    create user events

---
+++ Shop.Application/Users/Events/UserRegisteredHandler.cs
@@ -0,0 +1,25 @@
+namespace Shop.Application.Users.Events;
+
+using Interfaces;
+using Domain.Events;
+
+public class UserRegisteredHandler : INotificationHandler<UserRegistered>
+{
+    private readonly IUserService _userService;
+    private readonly IEmailService _emailService;
+
+    public UserRegisteredHandler(IUserService userService, IEmailService emailService)
+    {
+        _userService = userService;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(UserRegistered notification, CancellationToken cancellationToken)
+    {
+        var link = await _userService.GenerateConfirmationTokenAsync(notification.User.Id.Value);
+
+        var message = $"Click here to verify: {link}";
+
+        await _emailService.SendEmailAsync(notification.User.Email, "subject", message, cancellationToken);
+    }
+}
