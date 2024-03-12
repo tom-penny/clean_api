1bb9739310207400901cfedff017f310fb96f110
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 18:56:25 +0000

    create cache service

---
+++ Shop.Infrastructure/Caching/CacheService.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Shop.Infrastructure.Caching;
+
+using Application.Interfaces;
+
+public class CacheService : ICacheService
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public CacheService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public Task<Result<T>> GetAsync<T>(string key)
+    {
+        if (_memoryCache.TryGetValue(key, out T? value) && value != null)
+        {
+            return Task.FromResult(Result.Ok<T>(value));
+        }
+        
+        return Task.FromResult(Result.Fail<T>("Cache miss"));
+    }
+
+    public Task<Result> SetAsync<T>(string key, T value, TimeSpan expiry)
+    {
+        _memoryCache.Set(key, value, expiry);
+
+        return Task.FromResult(Result.Ok());
+    }
+
+    public async Task<Result<T>> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan expiry)
+    {
+        var value = await _memoryCache.GetOrCreateAsync(key, entry =>
+        {
+            entry.SetAbsoluteExpiration(expiry);
+            
+            return factory();
+        });
+        
+        return value != null ? Result.Ok(value) : Result.Fail("");
+    }
+}
