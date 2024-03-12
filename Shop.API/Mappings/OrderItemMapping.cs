65410b4300defc03c8e3348e4f4295203586e4aa
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 16:13:46 +0000

    create model mappings

---
+++ Shop.API/Mappings/OrderItemMapping.cs
@@ -0,0 +1,19 @@
+namespace Shop.API.Mappings;
+
+using Domain.Entities;
+using Models.Responses;
+
+public static class OrderItemMapper
+{
+    public static OrderItemResponse ToResponse(this OrderItem item)
+    {
+        return new OrderItemResponse
+        {
+            Id = item.Id.Value.ToString(),
+            OrderId = item.OrderId.Value.ToString(),
+            ProductId = item.ProductId.ToString(),
+            Quantity = item.Quantity,
+            UnitPrice = item.UnitPrice
+        };
+    }
+}
