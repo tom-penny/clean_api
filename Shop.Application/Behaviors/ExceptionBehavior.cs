d0981ed8d7f79f2989327b380b4ac0954d97da8d
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 19:04:57 +0000

    create exception behavior

---
+++ Shop.Application/Behaviors/ExceptionBehavior.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Logging;
+
+namespace Shop.Application.Behaviors;
+
+// Pipeline interceptor for Mediatr request exceptions.
+
+public class ExceptionBehavior<TRequest, TResponse> :
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<ExceptionBehavior<TRequest, TResponse>> _logger;
+
+    public ExceptionBehavior(ILogger<ExceptionBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "{@DateTime}: Failed {@Request}",
+                typeof(TRequest).Name, DateTime.UtcNow);
+
+            throw;
+        }
+    }
+}
