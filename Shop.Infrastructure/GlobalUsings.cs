c403b21237a95b6c8dd13b3f522f770852d8474e
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-12 19:30:04 +0000

    create global usings

---
+++ Shop.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using MediatR;
+global using FluentResults;
+global using System.Text;
+global using Microsoft.Extensions.Options;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.EntityFrameworkCore;
