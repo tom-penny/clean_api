8a2f9a20de36f39623186f2cb49877b398ebae8a
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-19 10:08:08 +0000

    add category slug

--- Shop.Domain/Entities/Category.cs
+++ Shop.Domain/Entities/Category.cs
@@ -1,13 +1,16 @@
+using System.Text.RegularExpressions;
+
 namespace Shop.Domain.Entities;
 
 using Primitives;
 
 public record CategoryId(Guid Value);
 
-public class Category : BaseEntity
+public partial class Category : BaseEntity
 {
     public CategoryId Id { get; init; }
     public string Name { get; private set; }
+    public string Slug => GenerateSlug();
 
     public Category(CategoryId id, string name)
     {
@@ -18,6 +21,11 @@ public class Category : BaseEntity
     public void Update(string name)
     {
         Name = name;
+    }
+
+    private string GenerateSlug()
+    {
+        return Regex.Replace(Name, "[^0-9A-Za-z _-]", string.Empty).ToLower().Replace(" ", "-");
     }
     
     private Category() {}
