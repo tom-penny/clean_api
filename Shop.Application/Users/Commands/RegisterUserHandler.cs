c4f710290c2e52bdc14bc58897cb0057ede01fc7
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-20 10:35:09 +0000

    update register user pipeline

--- Shop.Application/Users/Commands/RegisterUserHandler.cs
+++ Shop.Application/Users/Commands/RegisterUserHandler.cs
@@ -10,11 +10,13 @@ public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, Result<s
 {
     private readonly IApplicationDbContext _context;
     private readonly IUserService _userService;
+    private readonly IJwtProvider _jwtProvider;
     
-    public RegisterUserHandler( IApplicationDbContext context, IUserService userService)
+    public RegisterUserHandler(IApplicationDbContext context, IUserService userService, IJwtProvider jwtProvider)
     {
-        _userService = userService;
         _context = context;
+        _userService = userService;
+        _jwtProvider = jwtProvider;
     }
 
     public async Task<Result<string>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -44,7 +46,15 @@ public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, Result<s
         
             scope.Complete();
         }
+        
+        var loginResult = await _userService.SignInUserAsync(request.Email, request.Password);
 
-        return Result.Ok();
+        if (loginResult.IsFailed) return Result.Fail(UserError.InvalidCredentials());
+        
+        var roleResult = await _userService.GetRolesByIdAsync(loginResult.Value);
+        
+        var token = _jwtProvider.GenerateToken(loginResult.Value, request.Email, roleResult.Value);
+
+        return Result.Ok(token);
     }
 }
