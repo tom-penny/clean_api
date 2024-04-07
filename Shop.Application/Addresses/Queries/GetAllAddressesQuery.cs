f25e35f4dca95e6f4412be76fbc5e8031dcc9813
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-07 18:58:25 +0100

    update address queries

--- Shop.Application/Addresses/Queries/GetAllAddressesQuery.cs
+++ Shop.Application/Addresses/Queries/GetAllAddressesQuery.cs
@@ -3,7 +3,4 @@ namespace Shop.Application.Addresses.Queries;
 using Common.Interfaces;
 using Domain.Entities;
 
-public class GetAllAddressesQuery : IAuthorizedRequest<Result<List<Address>>>
-{
-    public required Guid UserId { get; init; }
-}
+public record GetAllAddressesQuery(Guid UserId) : IAuthorizedRequest<Result<List<Address>>>;
